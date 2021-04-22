76e469849b36c2b8fdd0096908912cecfe930d2e
limartin <lindsayjamesmartin94@gmail.com>
2021-04-22 19:16:34 +0200

    Added some libft style string comparisson functions

--- includes/general.h
+++ includes/general.h
@@ -1,9 +1,17 @@
 #ifndef GENERAL_H
 # define GENERAL_H
 
-int	*str_to_int_arrays(char **argv, int len);
-int	ft_is_numeric(const char *str);
-int	ft_atoi(const char *str);
-int	get_next_line(int fd, char **line);
+#include <stddef.h>
+
+int		*str_to_int_arrays(char **argv, int len);
+int		ft_is_numeric(const char *str);
+int		ft_atoi(const char *str);
+
+int		get_next_line(int fd, char **line);
+
+int		are_strs_eq(const char *s1, const char *s2);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
