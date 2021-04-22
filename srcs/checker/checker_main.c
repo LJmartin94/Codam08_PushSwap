7d4c9d11b41877fdaf5b689d3bbb6ee4f8369802
limartin <lindsayjamesmartin94@gmail.com>
2021-04-22 18:34:50 +0200

    Built the basic implementation for executing instructions in checker_main.c. Filled with junk functions and data currently.

--- srcs/checker/checker_main.c
+++ srcs/checker/checker_main.c
@@ -1,16 +1,40 @@
 #include "checker.h"
+#include <stdio.h>
+
+typedef int (*t_instruction)(int **, int*);
+
+int specific_function(int **stack, int *b_head)
+{
+	(void)stack;
+	(void)b_head;
+	printf("This shit actually works?!\n");
+	return (0);
+}
+
+t_instruction get_instruction(char *line)
+{
+	(void)line;
+	return (specific_function);
+}
 
 int	main(int argc, char **argv)
 {
 	int	*stack_a;
+	int b_head;
 	int run;
 	char *line;
+	int (*instruction)(int **, int *);
 
 	stack_a = str_to_int_arrays(argv, (argc - 1));
+	b_head = 0;
 	run = 1;
 	while (run)
 	{
 		run = get_next_line(1, &line);
+		if (line[0] == '\0')
+			break;
+		instruction = get_instruction(line);
+		instruction(&stack_a, &b_head);
 	}
 	return (0);
 }
