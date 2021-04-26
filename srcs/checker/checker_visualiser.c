ecd6b5673e53e69271382a540b2790292d3e7a48
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-04-26 14:07:20 +0200

    Wrote a visualiser. The code ain't too pretty, but the end result isn't bad for a quick job.

--- srcs/checker/checker_visualiser.c
+++ srcs/checker/checker_visualiser.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/26 12:57:44 by limartin      #+#    #+#                 */
-/*   Updated: 2021/04/26 13:28:20 by limartin      ########   odam.nl         */
+/*   Updated: 2021/04/26 14:03:19 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,24 @@ int	visualise_stacks(t_stack *stk)
 	int		i;
 
 	str_ary = int_array_to_strs(stk->stack, stk->total_len);
-	write(1, "A | B\n", 6);
+	write(1, "A          <-|->           B\n", 29);
 	i = 0;
 	while (i < stk->total_len)
 	{
-		write(1, "X | X\n", 6);
+		if (a(i, stk) >= stk->head && a(i, stk) < (stk->head + stk->a_len))
+		{
+			write(1, str_ary[a(i, stk)], ft_strlen(str_ary[a(i, stk)]));
+			write(1, "            ", 12 - ft_strlen(str_ary[a(i, stk)]));
+		}
+		else
+			write(1, "            ", 12);
+		write(1, " | ", 3);
+		if (b(i, stk) < stk->head && b(i, stk) >= 0)
+		{
+			write(1, str_ary[b(i, stk)], ft_strlen(str_ary[b(i, stk)]));
+			write(1, "            ", 12 - ft_strlen(str_ary[b(i, stk)]));
+		}
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
