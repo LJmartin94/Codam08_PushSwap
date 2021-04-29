a006e99a64d3ca61db823e48968a72177cb434ea
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-04-29 20:25:19 +0200

    Updated prototypes to match function changes that were already implemented elsewhere. Added prototype for multi-purpose merge sort function I still need to write.

--- includes/general.h
+++ includes/general.h
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/22 20:15:32 by lindsay       #+#    #+#                 */
-/*   Updated: 2021/04/26 13:27:10 by limartin      ########   odam.nl         */
+/*   Updated: 2021/04/29 20:21:36 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 # include <stdlib.h>
 # include "switch_case.h"
 
-int		*str_to_int_arrays(char **argv, int len);
+int		*str_to_int_arrays(char **strs, int len);
 int		ft_is_numeric(const char *str);
-int		ft_atoi(const char *str);
+int		mod_ft_atoi(const char *str);
 
 char	**int_array_to_strs(int *array, int len);
 char	*ft_itoa(int n);
@@ -30,5 +30,7 @@ int		are_strs_eq(const char *s1, const char *s2);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
+
+void 	*merge_sort(void *to_sort, int (*f)(void *, void *));
 
 #endif
