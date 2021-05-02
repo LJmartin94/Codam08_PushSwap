ecdd2e5a64614eb33af0b4342627a40cbce40c58
limartin <lindsayjamesmartin94@gmail.com>
2021-05-02 19:15:48 +0200

    Moved merge sort struct to header file

--- includes/general.h
+++ includes/general.h
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/22 20:15:32 by lindsay       #+#    #+#                 */
-/*   Updated: 2021/05/02 16:08:49 by lindsay       ########   odam.nl         */
+/*   Updated: 2021/05/02 18:32:36 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include "switch_case.h"
+
+typedef struct s_sort
+{
+	void	*to_sort;
+	int		*ans;
+	int		size;
+	int		items;
+	int		(*f)(struct s_sort *, void *, void *);
+} t_sort;
 
 int		*str_to_int_arrays(char **strs, int len);
 int		ft_is_numeric(const char *str);
@@ -31,6 +40,6 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 
-void 	*merge_sort(void *to_sort, int size, int len, int (*f)(void *, void *));
+void 	*merge_sort(void *to_sort, int size, int len, int (*f)(t_sort *, void *, void *));
 
 #endif
