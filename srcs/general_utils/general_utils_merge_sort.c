72b5c1d7d01ea43c1c73bc8650eff5c3bbaf0361
limartin <lindsayjamesmartin94@gmail.com>
2021-05-02 16:56:31 +0200

    Bundled all relevant merge_sort data into struct. Split array recursively into pairs of indices. Have not allocated more memory, just specified the indices to target.

--- srcs/general_utils/general_utils_merge_sort.c
+++ srcs/general_utils/general_utils_merge_sort.c
@@ -6,23 +6,50 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/29 18:54:23 by limartin      #+#    #+#                 */
-/*   Updated: 2021/04/29 21:55:06 by limartin      ########   odam.nl         */
+/*   Updated: 2021/05/02 16:39:47 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
+typedef struct s_sort
+{
+	int	*ans;
+	int size;
+	int items;
+	int (*f)(void *, void *);
+}	t_sort;
+
 #include <stdio.h>
 
-void 	*merge_sort(void *to_sort, int mem, int (*f)(void *, void *))
+int		ms_split(t_sort *d, void *to_sort, int first, int last)
 {
-	//void *swap;
-	int data_type;
+	int mid;
 
-	data_type = sizeof(to_sort)[0];
-	printf("Size: %d\n", data_type);
-	printf("Data: %d\n", ((int *)to_sort)[0]);
+	if ((last - first) > 1)
+	{
+		mid = ((last - first) / 2) + first;
+		ms_split(d, to_sort, first, mid);
+		ms_split(d, to_sort, mid + 1, last);
+	}
+	else
+	{
+		printf("F: %d L: %d\n", first, last);
+	}
+	return (0);
+}
 
-	f(((unsigned char)to_sort + 0 * mem), ((unsigned char)to_sort + 1 * mem));
+void 	*merge_sort(void *to_sort, int size, int len, int (*f)(void *, void *))
+{
+	t_sort data;
+
+	data.size = size;
+	data.items = len;
+	data.f = f;
+	ms_split(&data, to_sort, 0, len - 1);
+
+
+
+	// f(((unsigned char)to_sort + 0 * mem), ((unsigned char)to_sort + 1 * mem));
 	return (to_sort);
 }
