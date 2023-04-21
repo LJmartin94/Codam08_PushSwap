3ec89faa687a7b2b563bb52b2952c09fe30e813d
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-21 19:50:29 +0200

    Make header file norme

--- v2/push_swap.h
+++ v2/push_swap.h
@@ -6,20 +6,23 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 19:40:57 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 19:40:58 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/21 19:49:47 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h> //exit
-#include <unistd.h> //write
-#include <limits.h> //INT_MAX && INT_MIN
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h> //exit
+# include <unistd.h> //write
+# include <limits.h> //INT_MAX && INT_MIN
 
 //data struct -> anything that is malloc'd and needs to be freed goes in here.
 typedef struct s_data
 {
-	int *input;
-	int num_of_args;
-} t_data;
+	int	*input;
+	int	num_of_args;
+}	t_data;
 
 //utils.c
 int		ft_strlen(const char *str);
@@ -29,3 +32,5 @@ int		get_valid_type(char c);
 
 //parsing.c
 void	parsing(t_data *d, int argc, char **argv);
+
+#endif
