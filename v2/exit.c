b4b5f1a77d1fd11afd50416e4f37c2466311a01a
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-21 21:43:59 +0200

    Make exit free everything malloc'd in data structure. This means I need to change all existing calls to exit next, to ensure it gets the data structure.

--- v2/exit.c
+++ v2/exit.c
@@ -6,16 +6,24 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 21:38:00 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 21:38:44 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/21 21:42:01 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	free_all(t_data *d)
+{
+	if (d->input != NULL)
+		free(d->input);
+	d->input = NULL;
+}
+
 /* I like separating the output from the input with a newline, 
 but I'm sure the checker will hate it*/
-void	exit_with_message(char *msg, int error)
+void	exit_with_message(t_data *d, char *msg, int error)
 {
+	free_all(d);
 	write(1, "\n", 1);
 	if (error)
 		write(1, msg, ft_strlen(msg));
