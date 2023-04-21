a2506b5f5eaea8c90e888f16512bf7e02022b7fa
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-21 21:33:41 +0200

    Make main.c norme, except for the debug for-loop

--- v2/main.c
+++ v2/main.c
@@ -6,24 +6,21 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 19:41:04 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 19:59:49 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/21 21:32:57 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_data d;
+	t_data	d;
 
 	d.input = NULL;
 	d.num_of_args = 0;
 	parsing(&d, argc, argv);
-
-
 	for (int i = 0; i < d.num_of_args; i++)
 		printf("%d ", d.input[i]);
-	
-	return 0;
+	return (0);
 }
