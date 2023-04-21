d6d26637e13e4ef0ec5fb66704f51ac4b7ac107b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-21 19:47:05 +0200

    Make utils norme

--- v2/utils.c
+++ v2/utils.c
@@ -6,15 +6,15 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 19:40:52 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 19:40:54 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/21 19:46:45 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -22,37 +22,45 @@ int ft_strlen(const char *str)
 	return (i);
 }
 
-int get_valid_type(char c)
+/* Returns 'type' of character
+3 == sign
+2 == numeric
+1 == separator
+0 == invalid
+(enums would be nicer, but also longer so not norme) */
+int	get_valid_type(char c)
 {
-	if (c == '-' || c == '+') // is sign
-		return 3;
-	if (c >= '0' && c <= '9') // is numeric
-		return 2;
-	if (c == ' ' || c == '\0') // is separator
-		return 1;
-	return 0; // else is invalid
+	if (c == '-' || c == '+')
+		return (3);
+	if (c >= '0' && c <= '9')
+		return (2);
+	if (c == ' ' || c == '\0')
+		return (1);
+	return (0);
 }
 
+/* I like separating the output from the input with a newline, 
+but I'm sure the checker will hate it*/
 void	exit_with_message(char *msg, int error)
 {
-	write(1, "\n", 1); // I like separating the output from the input with a newline, but it might make the checker complain.
+	write(1, "\n", 1);
 	if (error)
 		write(1, msg, ft_strlen(msg));
 	exit(error);
 }
 
-// This is like a standard atoi but if it encounters an overlfow error it exits the program with an error message.
-// It also only accepts one sign and no leading spaces.
-int ft_modified_atoi(char *str)
+/* This is like a standard atoi but if it encounters an integer overflow error 
+it exits the program with an error message.
+It also does not accept leading spaces. */
+int	ft_modified_atoi(char *str)
 {
 	int			i;
 	long int	sign;
 	long int	res;
-	
+
 	i = 0;
 	sign = 1;
 	res = 0;
-
 	if ((str[i] == '-') || (str[i] == '+'))
 		i++;
 	if ((i > 0) && (str[i - 1] == '-'))
