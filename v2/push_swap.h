37aa8869b9d73ea2bbd078a597581cbc5a1a3915
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-21 18:13:27 +0200

    Move parsing to separate file, remove binary from repo

--- v2/push_swap.h
+++ v2/push_swap.h
@@ -12,3 +12,6 @@ int		ft_strlen(const char *str);
 void	exit_with_message(char *msg, int error);
 int		ft_atoi_with_exit(char *str);
 int		get_valid_type(char c);
+
+//parsing.c
+void	parsing(t_data d, int argc, char **argv);
