13926d98a6d1697c9e9fd678bce389340e53eba1
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-22 00:57:02 +0200

    Initialise data struct in separate function.

--- v2/main.c
+++ v2/main.c
@@ -6,23 +6,29 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 19:41:04 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 23:29:40 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/22 00:53:25 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
+void	initialise_data(t_data *d)
+{
+	d->input = NULL;
+	d->num_of_args = 0;
+	d->stack_a = NULL;
+	d->stack_b = NULL;
+	d->solution_a = NULL;
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	d;
 	int		i;
 
-	d.input = NULL;
-	d.num_of_args = 0;
-	d.stack_a = NULL;
-	d.stack_b = NULL;
-	d.solution_a = NULL;
+	initialise_data(&d);
 	parsing(&d, argc, argv);
 	i = 0;
 	while (i < d.num_of_args)
