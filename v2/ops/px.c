2ad2a7a4c6bd6d8ef9716ded7f329c4b9ae96668
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-28 17:47:18 +0200

    Make pushing from a stack with only one element work.

--- v2/ops/px.c
+++ v2/ops/px.c
@@ -6,31 +6,33 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/22 01:40:28 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/28 17:25:52 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/28 17:45:50 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 /* Pushes TO stack X (from the other stack) */
-void op_px(t_link **to, t_link *from)
+void op_px(t_link **to, t_link **from)
 {
 	t_link *to_move;
 	t_link *dest;
 
-	if (from == NULL)
+	if (*from == NULL)
 		return;
-	to_move = from;
+	to_move = *from;
 	while (to_move->next)
 		to_move = to_move->next;
 	if (to_move->previous != NULL)
 		to_move->previous->next = NULL;
+	else
+		*from = NULL;
 	dest = *to;
 	if (dest == NULL)
 		*to = to_move;
 	else
 	{
-		while (dest != NULL && dest->next != NULL)
+		while (dest->next)
 			dest = dest->next;
 		dest->next = to_move;
 	}	
@@ -39,12 +41,12 @@ void op_px(t_link **to, t_link *from)
 
 void pa(t_data *d)
 {
-	op_px(&d->stack_a, d->stack_b);
+	op_px(&d->stack_a, &d->stack_b);
 	return;
 }
 
 void pb(t_data *d)
 {
-	op_px(&d->stack_b, d->stack_a);
+	op_px(&d->stack_b, &d->stack_a);
 	return;
 }
