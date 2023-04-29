ee9a2e1237d54477f3e93de8087c7a9e2c12d6d8
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-29 17:47:33 +0200

    Write a linked list clean-up function

--- v2/exit.c
+++ v2/exit.c
@@ -6,17 +6,35 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 21:38:00 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/21 21:51:55 by limartin      ########   odam.nl         */
+/*   Updated: 2023/04/29 17:45:46 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	free_list(t_link *list)
+{
+	t_link *next_node;
+	
+	while(list != NULL)
+	{
+		next_node = list->next;
+		list->next = NULL;
+		list->previous = NULL;
+		free(list);
+		list = next_node;
+	}
+	return;
+}
 
 void	free_all(t_data *d)
 {
 	if (d->input != NULL)
 		free(d->input);
 	d->input = NULL;
+	free_list(d->stack_a);
+	free_list(d->stack_b);
+	free_list(d->solution_a);
 }
 
 /* I like separating the output from the input with a newline, 
