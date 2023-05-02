b6d0fbe20ed398a0f89bf9fe9ad62b5a60edb935
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-05-02 19:43:49 +0200

    Add a somewhat functional dumb_sort

--- v2/algos/dumb_sort.c
+++ v2/algos/dumb_sort.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/30 16:23:53 by limartin      #+#    #+#                 */
-/*   Updated: 2023/04/30 16:25:24 by limartin      ########   odam.nl         */
+/*   Updated: 2023/05/02 19:35:25 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,35 @@
 
 t_link *dumb_sort(t_data *d)
 {
-	t_link *solution;
+	t_link	*solution;
+	int		target;
+	int		pos;
+	t_link	*tmp;
 
-
+	while (stacks_sorted(d) != 2)
+	{
+		if (stacks_sorted(d) == 0)
+		{
+			target = d->num_of_args - size_of_list(d->stack_a);
+			tmp = d->stack_a;
+			pos = 0;
+			while (tmp && tmp->content != target)
+			{
+				tmp = tmp->below;
+				pos++;
+			}
+			if (d->stack_a && d->stack_a->content != target)
+			{
+				if (pos <= (size_of_list(d->stack_a) / 2))
+					ra(d);
+				else
+					rra(d);
+			}
+			else
+				pb(d);
+		}
+		else
+			pa(d);
+	}
 	return (solution);
 }
