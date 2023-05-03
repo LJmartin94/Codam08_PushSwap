03a74b4e642b4bd89f73f210bcdcc3b4666508fb
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-05-03 20:59:19 +0200

    Add set_or_reset stacks, which will initialise stack a to be a copy of the input received, and stack b to be empty, regardless of what state they may have been left in by the previous sorting algo.

--- v2/try_solutions.c
+++ v2/try_solutions.c
@@ -6,11 +6,33 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/30 16:12:07 by limartin      #+#    #+#                 */
-/*   Updated: 2023/05/03 16:43:10 by limartin      ########   odam.nl         */
+/*   Updated: 2023/05/03 20:47:32 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	set_or_reset_stacks(t_data *d)
+{
+	int i;
+
+	if (d->stack_a != NULL)
+	{
+		free_list(d->stack_a);
+		d->stack_a = NULL;
+	}
+	if (d->stack_b != NULL)
+	{
+		free_list(d->stack_b);
+		d->stack_b = NULL;
+	}
+	i = 0;
+	while (i < d->num_of_args)
+	{
+		d->stack_a = add_to_back(d->stack_a, new_node(d, d->input[i]));
+		i++;
+	}
+}
 
 void	try_solutions(t_data *d)
 {
@@ -19,6 +41,9 @@ void	try_solutions(t_data *d)
 
 	d->solutions = (t_link **)malloc(sizeof(t_link *) * SOLUTIONS_TOTAL);
 	d->solutions[DUMB_SORT] = dumb_sort(d);
+	printf("Dumb sort ops: %d\n", size_of_list(d->solutions[DUMB_SORT]));
+	d->solutions[RADIX_SORT] = radix_sort(d);
+	printf("Radix sort ops: %d\n", size_of_list(d->solutions[RADIX_SORT]));
 	index = 0;
 	shortest = 0;
 	while (index < SOLUTIONS_TOTAL)
