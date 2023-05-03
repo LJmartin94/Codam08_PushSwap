c5c9f9246f86569cf7034a9259d5ecac72a79597
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-05-03 18:16:17 +0200

    Make try solutions find the shortest solution and print it.

--- v2/try_solutions.c
+++ v2/try_solutions.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/30 16:12:07 by limartin      #+#    #+#                 */
-/*   Updated: 2023/05/03 14:33:54 by limartin      ########   odam.nl         */
+/*   Updated: 2023/05/03 16:43:10 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 void	try_solutions(t_data *d)
 {
+	int shortest;
+	int index;
+
 	d->solutions = (t_link **)malloc(sizeof(t_link *) * SOLUTIONS_TOTAL);
 	d->solutions[DUMB_SORT] = dumb_sort(d);
+	index = 0;
+	shortest = 0;
+	while (index < SOLUTIONS_TOTAL)
+	{
+		if (size_of_list(d->solutions[index]) < \
+			size_of_list(d->solutions[shortest]))
+			shortest = index;
+		index++;
+	}
+	write_solution(d->solutions[shortest]);
 }
