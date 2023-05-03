0cada946fea3416c7efe58f5a09df1db2cb2908a
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-05-03 18:16:47 +0200

    Add all prototypes and enums

--- v2/push_swap.h
+++ v2/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 19:40:57 by limartin      #+#    #+#                 */
-/*   Updated: 2023/05/03 14:32:04 by limartin      ########   odam.nl         */
+/*   Updated: 2023/05/03 16:42:38 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,17 @@ enum e_solution
 enum e_operation
 {
 	ERROR,
+	PA,
+	PB,
+	SA,
+	SB,
+	SS,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR,
 	OPS_TOTAL,
 };
 
@@ -51,54 +62,60 @@ typedef struct s_data
 }	t_data;
 
 //utils.c
-int		ft_strlen(const char *str);
-int		ft_modified_atoi(t_data *d, char *str);
-int		stacks_sorted(t_data *d);
+int					ft_strlen(const char *str);
+int					ft_modified_atoi(t_data *d, char *str);
+
+//solution_utils.c
+int					stacks_sorted(t_data *d);
+t_link				*apply_op(t_data *d, \
+					enum e_operation (*op)(t_data *d), \
+					t_link *solution);
+void				write_solution(t_link *solution);
 
 //parsing.c
-void	parsing(t_data *d, int argc, char **argv);
+void				parsing(t_data *d, int argc, char **argv);
 
 //sort_input.c
-void	translate_input(t_data *d);
+void				translate_input(t_data *d);
 
 //linked_lists.c
-int		size_of_list(t_link *to_count);
-t_link	*new_node(t_data *d, int content);
-t_link	*add_to_back(t_link *head, t_link *to_add);
-t_link	*copy_list(t_data *d, t_link *to_copy);
+int					size_of_list(t_link *to_count);
+t_link				*new_node(t_data *d, int content);
+t_link				*add_to_back(t_link *head, t_link *to_add);
+t_link				*copy_list(t_data *d, t_link *to_copy);
 
 //try_solutions.c
-void	try_solutions(t_data *d);
+void				try_solutions(t_data *d);
 
 //algos
-t_link	*dumb_sort(t_data *d);
+t_link				*dumb_sort(t_data *d);
 
 //ops
-void	op_px(t_link **to_stack, t_link **from_stack);
-void	pa(t_data *d);
-void	pb(t_data *d);
+int					op_px(t_link **to_stack, t_link **from_stack);
+enum e_operation	pa(t_data *d);
+enum e_operation	pb(t_data *d);
 
-void	op_sx(t_link **stack);
-void	sa(t_data *d);
-void	sb(t_data *d);
-void	ss(t_data *d);
+int					op_sx(t_link **stack);
+enum e_operation	sa(t_data *d);
+enum e_operation	sb(t_data *d);
+enum e_operation	ss(t_data *d);
 
-void	op_rx(t_link **stack);
-void	ra(t_data *d);
-void	rb(t_data *d);
-void	rr(t_data *d);
+int					op_rx(t_link **stack);
+enum e_operation	ra(t_data *d);
+enum e_operation	rb(t_data *d);
+enum e_operation	rr(t_data *d);
 
-void	op_rrx(t_link **stack);
-void	rra(t_data *d);
-void	rrb(t_data *d);
-void	rrr(t_data *d);
+int					op_rrx(t_link **stack);
+enum e_operation	rra(t_data *d);
+enum e_operation	rrb(t_data *d);
+enum e_operation	rrr(t_data *d);
 
 //exit.c
-void	exit_with_message(t_data *d, char *msg, int error);
+void				exit_with_message(t_data *d, char *msg, int error);
 
 //debug.c -> functions and includes to be removed before handing in.
 # include <stdio.h>
 
-void	print_list(t_link *to_print);
+void				print_list(t_link *to_print);
 
 #endif
